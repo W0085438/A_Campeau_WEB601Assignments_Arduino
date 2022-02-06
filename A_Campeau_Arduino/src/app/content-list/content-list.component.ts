6bb68bd601ef90055d94bcc25572a9c30c45f445
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-06 16:16:57 -0500

    Added "clicked" function

--- A_Campeau_Arduino/src/app/content-list/content-list.component.ts
+++ A_Campeau_Arduino/src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
-
+  @Input() contentItem?: Content;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  clicked(){
+    if(this.contentItem != null){
+      console.log(`Title: '${this.contentItem.title}' \nId: '${this.contentItem.id}'`);
+    }
+  }
 }
+
