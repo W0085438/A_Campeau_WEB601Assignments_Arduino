e8ff26c27454246e9908806d36e50b01c22b76d4
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-06 17:30:44 -0500

    Added images and data

--- A_Campeau_Arduino/src/app/app.component.ts
+++ A_Campeau_Arduino/src/app/app.component.ts
@@ -15,46 +15,46 @@ export class AppComponent {
       {
         id: 0,
         title: "UNO",
-        description: "",
-        creator: "",
+        description: "Entry level board",
+        creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000066_03.front_970c6014-61ab-4226-a20f-14cc6d8d682c_1000x750.jpg?v=1629816078",
-        type: "",
+        type: "Microcontroller",
         tags: []      
       },
       {
         id: 0,
         title: "MEGA 2560",
-        description: "",
-        creator: "",
+        description: "Expanded IO board",
+        creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000067_00.front_1000x750.jpg?v=1637830354",
-        type: "",
+        type: "Microcontroller",
         tags: []      
       },
       {
         id: 0,
         title: "DUE",
-        description: "",
+        description: "Powerful, intermediate board",
         creator: "",
-        imgURL: "",
-        type: "",
+        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000062_01.iso_c7d197dc-56b9-4e5e-80fd-6cdc44c007ae_1000x750.jpg?v=1637829938",
+        type: "Microcontroller",
         tags: []      
       },
       {
         id: 0,
         title: "NANO",
-        description: "",
-        creator: "",
-        imgURL: "",
-        type: "",
+        description: "Small form factor",
+        creator: "Arduino",
+        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000005_03.front_2c44e4db-5d01-45ec-9b14-78b6df214913_1000x750.jpg?v=1628695116",
+        type: "Microcontroller",
         tags: []      
       },
       {
         id: 0,
         title: "LEONARDO",
-        description: "",
-        creator: "",
-        imgURL: "",
-        type: "",
+        description: "Improved connectivity",
+        creator: "Arduino",
+        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000057_03.front_710x467.jpg?v=1626444500",
+        type: "Microcontroller",
         tags: []      
       },
     ]
