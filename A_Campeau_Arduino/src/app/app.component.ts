7c59983e36ec071152c7cd382d5663b591418a52
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-06 17:22:33 -0500

    Fixed image link

--- A_Campeau_Arduino/src/app/app.component.ts
+++ A_Campeau_Arduino/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
         title: "UNO",
         description: "",
         creator: "",
-        imgURL: "https://store-usa.arduino.cc/collections/boards/products/arduino-uno-rev3",
+        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000066_03.front_970c6014-61ab-4226-a20f-14cc6d8d682c_1000x750.jpg?v=1629816078",
         type: "",
         tags: []      
       },
