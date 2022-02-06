ea9477d6644f829bab8de0a2aee9fb141785639b
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-06 16:51:31 -0500

    Added items to AppComponent

--- A_Campeau_Arduino/src/app/app.component.ts
+++ A_Campeau_Arduino/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'A_Campeau_Arduino';
+  title = 'About Arduino (working title)';
+  contentList: Content[];
+
+  constructor(){
+    this.contentList = [
+      {
+        id: 0,
+        title: "UNO",
+        description: "",
+        creator: "",
+        imgURL: "",
+        type: "",
+        tags: []      
+      },
+      {
+        id: 0,
+        title: "MEGA 2560",
+        description: "",
+        creator: "",
+        imgURL: "",
+        type: "",
+        tags: []      
+      },
+      {
+        id: 0,
+        title: "DUE",
+        description: "",
+        creator: "",
+        imgURL: "",
+        type: "",
+        tags: []      
+      },
+      {
+        id: 0,
+        title: "NANO",
+        description: "",
+        creator: "",
+        imgURL: "",
+        type: "",
+        tags: []      
+      },
+      {
+        id: 0,
+        title: "LEONARDO",
+        description: "",
+        creator: "",
+        imgURL: "",
+        type: "",
+        tags: []      
+      },
+    ]
+  }
 }
