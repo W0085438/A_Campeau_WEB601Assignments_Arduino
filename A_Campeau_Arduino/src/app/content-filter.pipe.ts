27c034a5528eff08031a99a86ef15708c510c815
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-13 14:15:05 -0500

    Built out pipe

--- A_Campeau_Arduino/src/app/content-filter.pipe.ts
+++ A_Campeau_Arduino/src/app/content-filter.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'contentFilter'
 })
 export class ContentFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(contentItem: Content[], htype?: string,): Content[] {
+
+    let filter;
+
+    if(!htype){
+      filter = contentItem.filter(item => item.type == "" || item.type == null);
+    } else {
+      filter = contentItem.filter(item => item.type == htype);
+    }
+    return filter;
   }
 
 }
