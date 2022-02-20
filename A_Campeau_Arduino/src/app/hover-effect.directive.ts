753c57241ca78a9afb4a4e129620a02214bc1a3c
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-20 17:53:37 -0500

    Built out directive

--- A_Campeau_Arduino/src/app/hover-effect.directive.ts
+++ A_Campeau_Arduino/src/app/hover-effect.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverEffect]'
 })
 export class HoverEffectDirective {
+  @Input() styleType?: string;
 
-  constructor() { }
+  constructor(private elem: ElementRef) { 
+    this.elem.nativeElement.style.cursor = "pointer";
+  }
+
+  @HostListener('mouseover') onHover(typeElem: any){
+    this.elem.nativeElement.style.textDecoration = this.styleType ?? "none";
+    this.elem.nativeElement.style.fontWeight = this.styleType ?? "normal";
+  }
+
+  @HostListener('mouseover') offHover(typeElem: any){
+    this.elem.nativeElement.style.textDecoration = "none";
+    this.elem.nativeElement.style.fontWeight = "normal";
+  }
 
 }
