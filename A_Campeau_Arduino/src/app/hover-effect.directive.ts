02dbd8040f24e4682b26d02d28fe00c37529ef54
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-20 21:41:19 -0500

    Fix directive

--- A_Campeau_Arduino/src/app/hover-effect.directive.ts
+++ A_Campeau_Arduino/src/app/hover-effect.directive.ts
@@ -14,11 +14,11 @@ export class HoverEffectDirective implements OnInit {
   }
 
   @HostListener('mouseover') onHover(typeElem: any){
-    this.elem.nativeElement.style.textDecoration = this.styleType ?? "underline";
+    this.elem.nativeElement.style.textDecoration = this.styleType ?? "none";
     this.elem.nativeElement.style.fontWeight = this.styleType ?? "normal";
   }
 
-  @HostListener('mouseover') offHover(typeElem: any){
+  @HostListener('mouseout') offHover(typeElem: any){
     this.elem.nativeElement.style.textDecoration = "none";
     this.elem.nativeElement.style.fontWeight = "normal";
   }
