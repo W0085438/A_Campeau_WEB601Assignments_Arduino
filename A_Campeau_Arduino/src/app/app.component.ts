af5d20b0d494abc487e4f6eb299e38ad3171d6e2
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-20 20:10:09 -0500

    Update properties

--- A_Campeau_Arduino/src/app/app.component.ts
+++ A_Campeau_Arduino/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000066_03.front_970c6014-61ab-4226-a20f-14cc6d8d682c_1000x750.jpg?v=1629816078",
         type: "Microcontroller",
-        tags: ['Electronics', 'Project']      
+        tags: ['electronics', 'project']      
       },
       {
         id: 1,
@@ -27,7 +27,7 @@ export class AppComponent {
         description: "Expanded IO board",
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000067_00.front_1000x750.jpg?v=1637830354",
-        type: "Microcontroller",    
+        type: "Microcontroller"    
       },
       {
         id: 2,
@@ -36,7 +36,7 @@ export class AppComponent {
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000062_01.iso_c7d197dc-56b9-4e5e-80fd-6cdc44c007ae_1000x750.jpg?v=1637829938",
         type: "",
-        tags: ['Electronics', 'Project']      
+        tags: ['electronics', 'project']      
       },
       {
         id: 3,
@@ -45,7 +45,7 @@ export class AppComponent {
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000005_03.front_2c44e4db-5d01-45ec-9b14-78b6df214913_1000x750.jpg?v=1628695116",
         type: "Microcontroller",
-        tags: ['Electronics', 'Project']      
+        tags: ['electronics', 'project']      
       },
       {
         id: 4,
@@ -54,15 +54,14 @@ export class AppComponent {
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000057_03.front_710x467.jpg?v=1626444500",
         type: "Microcontroller+",
-        tags: ['Electronics', 'Project']      
+        tags: ['electronics', 'project']      
       },
       {
         id: 5,
         title: "ZERO",
         description: "Intermediate board",
         creator: "Arduino",
-        //imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/ABX00003_03.front_92550441-6d96-4e50-9268-5718c39ce4d1_2130x1400.jpg?v=1627483216",
-        type: "Microcontroller",     
+        type: "Microcontroller"     
       },
       {
         id: 6,
@@ -71,7 +70,7 @@ export class AppComponent {
         creator: "Arduino",
         imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/ABX00003_03.front_92550441-6d96-4e50-9268-5718c39ce4d1_2130x1400.jpg?v=1627483216",
         type: "Microcontroller+",
-        tags: ['Electronics', 'Project']      
+        tags: ['electronics', 'project']      
       }
     ]
   }
