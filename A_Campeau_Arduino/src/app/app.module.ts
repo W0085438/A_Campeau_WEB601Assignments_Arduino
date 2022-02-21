fe03a9fcbbbc4c609c999906385ecb56232a9b3a
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-02-20 21:09:31 -0500

    Removed ContentCard import

--- A_Campeau_Arduino/src/app/app.module.ts
+++ A_Campeau_Arduino/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentFilterPipe } from './content-filter.pipe';
 import { HoverEffectDirective } from './hover-effect.directive';
