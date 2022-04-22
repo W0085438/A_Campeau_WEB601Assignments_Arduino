c4cb4908729cefbf83200fdbc7b667925fe04498
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-04-22 14:59:26 -0400

    created new component

--- A_Campeau_Arduino/src/app/app.module.ts
+++ A_Campeau_Arduino/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentFilterPipe } from './content-filter.pipe';
 import { HoverEffectDirective } from './hover-effect.directive';
+import { CreateContentComponent } from './create-content/create-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentFilterPipe,
-    HoverEffectDirective
+    HoverEffectDirective,
+    CreateContentComponent
   ],
   imports: [
     BrowserModule
