bd1c8c2abcbae5799104e69894468f79e0bf0988
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-04-23 04:10:20 -0400

    updated interface

--- A_Campeau_Arduino/src/app/modify-content/modify-content.component.ts
+++ A_Campeau_Arduino/src/app/modify-content/modify-content.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { ArduinoServiceService } from '../arduino-service.service';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-modify-content',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modify-content.component.scss']
 })
 export class ModifyContentComponent implements OnInit {
+  @Output() newBoardEvent: EventEmitter<Content> = new EventEmitter<Content>();
+  @Output() updateBoardEvent: EventEmitter<Content> = new EventEmitter<Content>();
+  content?: Content;
 
-  constructor() { }
+  constructor(private arduinoService: ArduinoServiceService) { }
 
   ngOnInit(): void {
+  }
+
+  addBoard(title: string, description: string, creator: string, imgUrl: string, type: string, tags: string) {
+    this.content = {
+      title: title,
+      description: description,
+      creator: creator,
+      imgURL: imgUrl,
+      type: type,
+      tags: tags.split(', ')
+    }
+    this.newBoardEvent.emit(this.content);
+  }
+
+  updateBoard(title: string, description: string, creator: string, imgUrl: string, type: string, tags: string) {
+    this.content = {
+      title: title,
+      description: description,
+      creator: creator,
+      imgURL: imgUrl,
+      type: type,
+      tags: tags.split(',')
+    }
+    this.updateBoardEvent.emit(this.content);
   }
 
 }
