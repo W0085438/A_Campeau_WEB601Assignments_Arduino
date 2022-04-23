e88e755771bd3da13acb4bab86c1e6393bb3d7ae
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-04-23 05:24:57 -0400

    Fixed variable naming

--- A_Campeau_Arduino/src/app/services/in-memory-data.service.ts
+++ A_Campeau_Arduino/src/app/services/in-memory-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Content } from '../helper-files/content-interface';
-import { boardList } from '../helper-files/board-list';
+import { BOARDLIST } from '../helper-files/contentDb';
 
 @Injectable({
   providedIn: 'root'
