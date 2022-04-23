82fbb13f77cc333f07138f9f7309768e1231d6cc
W0085438 <90987029+W0085438@users.noreply.github.com>
2022-04-23 01:54:09 -0400

    updated app-component

--- A_Campeau_Arduino/src/app/app.component.ts
+++ A_Campeau_Arduino/src/app/app.component.ts
@@ -9,79 +9,30 @@ import { boardList } from './helper-files/board-list'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'About Arduino (working title)';
-  contentList: Content[];
+  boardList: Content[];
+  filteredBoard: Content[];
 
-  constructor(){
-    this.contentList = [
-      {
-        id: 0,
-        title: "UNO",
-        description: "Entry level board",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000066_03.front_970c6014-61ab-4226-a20f-14cc6d8d682c_1000x750.jpg?v=1629816078",
-        type: "Microcontroller",
-        tags: ['electronics', 'project']      
-      },
-      {
-        id: 1,
-        title: "MEGA 2560",
-        description: "Expanded IO board",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000067_00.front_1000x750.jpg?v=1637830354",
-        type: "Microcontroller"    
-      },
-      {
-        id: 2,
-        title: "DUE",
-        description: "Powerful, intermediate board",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000062_01.iso_c7d197dc-56b9-4e5e-80fd-6cdc44c007ae_1000x750.jpg?v=1637829938",
-        tags: ['electronics', 'project']      
-      },
-      {
-        id: 3,
-        title: "NANO",
-        description: "Small form factor",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000005_03.front_2c44e4db-5d01-45ec-9b14-78b6df214913_1000x750.jpg?v=1628695116",
-        type: "Microcontroller",
-        tags: ['electronics', 'project']      
-      },
-      {
-        id: 4,
-        title: "LEONARDO",
-        description: "Improved connectivity",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/A000057_03.front_710x467.jpg?v=1626444500",
-        type: "Microcontroller+",
-        tags: ['electronics', 'project']      
-      },
-      {
-        id: 5,
-        title: "ZERO",
-        description: "Intermediate board",
-        creator: "Arduino",
-        type: "Microcontroller"     
-      },
-      {
-        id: 6,
-        title: "ZERO II",
-        description: "Intermediate board",
-        creator: "Arduino",
-        imgURL: "https://cdn.shopify.com/s/files/1/0506/1689/3647/products/ABX00003_03.front_92550441-6d96-4e50-9268-5718c39ce4d1_2130x1400.jpg?v=1627483216",
-        type: "Microcontroller+",
-        tags: ['electronics', 'project']      
-      }
-    ]
+
+  constructor(private arduinoService: ArduinoServiceService, public messageService: MessageService){
+    this.boardList = [];
+    this.filteredBoard = [];
   }
-  filter(cardTitle: string, contentList: Content[]): string {
-    for(let item of contentList){
-      if (cardTitle == item.title){
-        return "There is an item with that title:";
-      }
+
+  ngOnInit(): void {
+    this.arduinoService.getBoardsObs().subscribe(boardArray => this.boardList = boardArray);
+    this.arduinoService.getIndividualBoard(3).subscribe(individualBoard => this.filteredBoard = individualBoard);
+  }
+
+  getBoard(id: string) {
+    let newId = parseInt(id);
+    if(isNaN(newId)){
+      return this.messageService.add(`A number between 0 and ${this.boardList.length - 1}`);
+    } else {
+      this.messageService.clear();
+      return this.arduinoService.getIndividualBoard(newId).subscribe(individualBoard => this.filteredBoard = individualBoard);
     }
-        return "Sorry, there are no items with that title!";
   }
+
 }
